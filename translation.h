0e5f3420b7ea70460e0941c017f2dd1bb32d7066
jinyangustc <jinyangustc@gmail.com>
2013-07-25 16:04:21 +0800

    fix bugs
    Owing to the DNA sequences that our database provides are the
    UNEXPRESSION strands, the translation process of the program can just
    use DNA sequences without transcription. It should be noted that users
    have to enable the transcription module if their databases use
    different strands of DNA sequences.

--- translation.h
+++ translation.h
@@ -19,26 +19,46 @@ public:
     void initializeGeneSequence( std::string sequence,std::string RNAsequence,int number ){
         geneNumber = number;
         geneSequence += sequence;
-        RNASequence +=RNAsequence;
+        //RNASequence +=RNAsequence;
     }
     void initializeGeneSequence( std::string sequence,int number ){
         geneNumber = number;
         geneSequence += sequence;
-        RNASequence +=sequence;
+        //RNASequence +=sequence;
     }
     //get gene amino acid sequence;
     std::string getAminoAcidSequence(){
-        transcription();
+        //transcription();
         translation();
         return aminoAcidSequence;
     }
 private:
     std::string geneSequence;
-    std::string RNASequence;
+    //std::string RNASequence;
     std::string aminoAcidSequence;
     int geneNumber;
+
+//****************************************************************************************
+//  Some explain of the transcription and translation process:
+//    1.Actually, the protein expression process is:
+//      DNA --> mRNA (i.e. transcription);
+//      mRNA --> protein (i.e. translation).
+//    2.DNA has double strands, but only one strand takes part in transcription.
+//    3.Codons are the sequence messages carried by mRNA;
+//      Take initiation codon "AUG" for example:
+//      ---ATG--- : DNA strand which doesn't take part in transcription process;
+//      ---TAC--- : DNA strand which exactlly takes part in transcription proess;
+//      ---AUG--- : mRNA strand which carries codons. In this case, it carries initiation
+//                 codon, i.e. "AUG";
+//      ---TAC--- : tRNA which also carries amino acid Methionine(M);
+//    4.Owing to the DNA sequences that our database provides are the UNEXPRESSION
+//      strands, the translation process of the program can just use DNA sequence without
+//      transcription. It should be noted that users have to enable the transcription
+//      module if their databases use different strands of DNA sequences.
+//****************************************************************************************
+   
     //transcript gene sequence(DNA) into RNA sequence;
-    void transcription(){
+    /*void transcription(){
         for(int i = 1; i != geneSequence.size(); i++){
             RNASequence[i] = transcript(geneSequence[i]);
         }
@@ -49,22 +69,24 @@ private:
         else if(s == 'A') return 'U';
         else if(s == 'T') return 'A';
         else if(s == 'C') return 'G';
-    }
+    }*/
+
     //translate RNA sequence into amino acid sequence;
     void translation(){
         //amino acid codon;
-        //U = 0; C = 1; A = 2; G = 3;
+        //U or T = 0; C = 1; A = 2; G = 3;
         char codon[4][4][4] = {{{'F', 'F', 'L', 'L'}, {'S', 'S', 'S', 'S'}, {'Y', 'Y', '#', '#'}, {'C', 'C', '#', 'W'}}, {{'L', 'L', 'L', 'L'}, {'P', 'P', 'P', 'P'}, {'H', 'H', 'Q', 'Q'}, {'R', 'R', 'R', 'R'}}, {{'I', 'I', 'I', 'M'}, {'T', 'T', 'T', 'T'}, {'N', 'N', 'K' ,'K'}, {'S', 'S', 'R', 'R'}}, {{'V', 'V', 'V', 'V'}, {'A', 'A', 'A', 'A'}, {'D', 'D', 'E', 'E'}, {'G', 'G', 'G', 'G'}}};
-        char tRNA[3];
+        char mRNA[3];
         int index[3] = { 0 };
         aminoAcidSequence = " ";
-        for (int i = 1; i != RNASequence.size(); i += 3) {
-            tRNA[0] = RNASequence[i];
-            tRNA[1] = RNASequence[i + 1];
-            tRNA[2] = RNASequence[i + 2];
+        //If need to use transcription module, replace geneSequence with RNASquence; 
+        for (int i = 1; i != geneSequence.size(); i += 3) {
+            mRNA[0] = geneSequence[i];
+            mRNA[1] = geneSequence[i + 1];
+            mRNA[2] = geneSequence[i + 2];
             //change A, U, C, G into number;
             for (int j = 0; j != 3; ++j) {
-                index[j] = translate(tRNA[j]);
+                index[j] = translate(mRNA[j]);
             }
         if (codon[index[0]][index[1]][index[2]] != '#') {
             aminoAcidSequence += codon[index[0]][index[1]][index[2]];
@@ -72,7 +94,10 @@ private:
         }
     }
     int translate(char s){
-        if (s == 'U') {
+        /*if (s == 'U') {
+            return 0;
+        }*/                //If need to use transcription module, enable this;
+        if (s == 'T') {
             return 0;
         }
         if (s == 'C') {
